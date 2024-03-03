0583bb7aa205ca4f3cac69fd702273166771734f
Aritra Dalal <aritra.dalal@gmail.com>
2024-03-03 02:50:54 +0000

    inserted first html file

--- index.mjs
+++ index.mjs
@@ -7,7 +7,22 @@ import fs from 'fs';
 const server = http.createServer((req, res) => {
     switch (req.method) {
         case 'GET':
-            res.end('GET');
+            switch(req.url) {
+                case '/':
+                    const filePath = path.join(__dirname, 'html', 'index.html', (err, data) => {
+                        if(err) {
+                            res.writeHead(500, {'Content-Type': 'text/plain'});
+                            res.end('Internal Server Error');
+                        } else {
+                            res.writeHead(200, {'Content-Type': 'text/html'});
+                            res.end(data);
+                        }
+                    });
+                    break;
+                default:
+                    res.end('Not Found');
+                    break;
+            }
             break;
         case 'POST':
             res.end('POST');
