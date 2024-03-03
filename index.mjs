4a590978b6421b723d4627c8bcebfa20084f88f9
Aritra Dalal <aritra.dalal@gmail.com>
2024-03-03 19:28:30 +0000

    First contents - pending corrections

--- index.mjs
+++ index.mjs
@@ -3,8 +3,22 @@ import http from 'http';
 import postgres from 'postgres';
 import fs from 'fs';
 import path from 'path';
+import ejs from 'ejs';
+import mime from 'mime';
 
 const html = 'html';
+const cachecleanupms = 5 * 60 * 1000;
+
+function readEJSFile(urlPath, callback) {
+    fs.readFile(path.join(html, urlPath), (err, data) => {
+        if(err) {
+            callback(err);
+        } else {
+            const rendered = ejs.render(data);
+            callback(null, rendered);
+        }
+    });
+}
 
 function readFile(urlPath, callback) {
     fs.readFile(path.join(html, urlPath), (err, data) => {
@@ -20,29 +34,54 @@ function readFile(urlPath, callback) {
 const server = http.createServer((req, res) => {
     switch (req.method) {
         case 'GET':
-            switch(req.url) {
-                case '/':
-                    readFile('index.html', (err, data) => {
-                        if(err) {
-                            res.writeHead(500, { 'Content-Type': 'text/plain'});
-                            res.end('Internal Server Error');
-                        } else {
-                            res.writeHead(200, { 'Content-Type': 'text/html'});
-                            res.end(data);
-                        }
-                    });
-                    break;
-                default:
-                    res.writeHead(404, { 'Content-Type': 'text/plain'});
-                    res.end('File Not Found');
-                    break;
-            }
+            if(req.url === '/') {
+                readEJSFile('index.html', (err, data) => {
+                    if(err) {
+                        res.writeHead(500, { 'Content-Type': 'text/plain' });
+                        res.end('Internal Server Error');
+                    } else {
+                        res.writeHead(200, { 'Content-Type': 'text/html' });
+                        res.end(data);
+                    }
+                });
+            } else if(req.url.startWith('/css')) {
+                readFile(req.url.pathname.replace("/^\//g, ''"), (err, data) => {
+                    if(err) {
+                        res.writeHead(404, { 'Content-Type': 'text/plain' });
+                        res.end('Not Found');
+                    } else {
+                        res.writeHead(200, { 'Content-Type': 'text/css' });
+                        res.end(data)
+                    }
+                })
+            } else if(req.url.startWith('/scripts')) {
+                readFile(req.url.pathname.replace("/^\//g, ''"), (err, data) => {
+                    if(err) {
+                        res.writeHead(404, { 'Content-Type': 'text/plain' });
+                        res.end('Not Found');
+                    } else {
+                        res.writeHead(200, { 'Content-Type': 'text/javascript' });
+                        res.end(data)
+                    }
+                })
+            } else if(req.url.startWith('/images')) {
+                readFile(req.url.pathname.replace("/^\//g, ''"), (err, data) => {
+                    if(err) {
+                        res.writeHead(404, { 'Content-Type': 'text/plain' });
+                        res.end('Not Found');
+                    } else {
+                        res.writeHead(200, { 'Content-Type': mime.getExtension(req.url.pathname) });
+                        res.end(data)
+                    }
+                }) 
+            } else if(req.url.startsWith('/')) {
+            } else {}
             break;
         case 'POST':
             res.end('POST');
             break;
         default:
-            res.end('Not Found');
+            res.writeHead(405)
             break;
     }
 });
