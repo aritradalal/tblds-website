80eb6c85e4a0e09d0073e5f010fe4dce3490e6a8
Aritra Dalal <aritra.dalal@gmail.com>
2024-03-07 21:43:29 +0000

    error logging

--- index.mjs
+++ index.mjs
@@ -39,6 +39,7 @@ const server = http.createServer((req, res) => {
                     if(err) {
                         res.writeHead(500, {'Content-Type': 'text/plain'});
                         res.end('500 Internal Server Error');
+                        console.error(err);
                     } else {
                         res.writeHead(200, {'Content-Type': 'text/html'});
                         res.end(data);
